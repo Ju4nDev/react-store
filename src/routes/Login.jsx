636cf0225b22a705dde8b7a61f2352977608bcae
Juan <juanpfreire18@gmail.com>
2024-04-10 14:38:18 -0300

    Criando o componente Login

---
+++ src/routes/Login.jsx
@@ -0,0 +1,12 @@
+export default function Login() {
+    return(
+        <>
+            <div className="container">
+                <h1>Login</h1>
+                <input type="text"></input>
+                <input type="password"></input>
+            </div>
+        </>
+    )
+}
+
